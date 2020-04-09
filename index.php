9da387905d1b362f30ebfb2e829b4003dd83ab82
Alaedine Benani D <43844475+alaedinebe@users.noreply.github.com>
2020-04-09 11:57:06 +0200

    basics

--- index.php
+++ index.php
@@ -1,12 +1,101 @@
 <!DOCTYPE html>
 <html>
+
 <head>
+	<!-- Pour le Navigateur -->
+	<title>ELISA</title>
+
+	<!-- Pour Google -->
+	<meta name="ELISA" content="Le JARVIS du chercheur"/>
+	<meta charset="utf-8">
+
+	<!-- Pour Facebook -->
+	<meta property="og:title" content="ELISA, le JARVIS des chercheurs">
+	<meta property="og:image" content="facebook-card.jpg">
+	<meta property="og:description" content="ELISA est un algorithme qui résume les articles scientifiques concernant Covid-19."/>
+	<meta property="og:site_name" content="ELISA"/>
+
+	<!-- Pour utiliser Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+
+	<!-- Pour utiliser la police-->
+	<link href="https://fonts.googleapis.com/css?family=Nunito:300,400,700" rel="stylesheet">
+
+	<!-- Pour lier le style.css -->
+	<link rel="stylesheet" href="style.css" />
+
+	<!-- Pour lier le favicon -->
+	<link rel="icon" type="image/png" href="img/logo.png" />
+
 </head>
 
 <body>
 
-<h1>My First Heading</h1>
-<p>My first paragraph.</p>
+	<div id="banner">
+		<h1> <!-- <img src="img/Logo_page.png" height="80px"> --> E.L.I.S.A.</h1>
+	    <p>Extensive Lectures of Informations and Scientific Articles.</p>
+
+	    <div id="button">
+	    <p>
+	      <a href="sign_up.html">En savoir plus !</a>
+	    </p>
+		</div>
+
+    </div>
+
+    <div id="card-list">
+
+    	<div class="card">		
+		    <img src="img/.svg" alt="" height="140">
+		    <h2>Nature</h2>
+		    <p>Et on arrive à l'heure, <strong>sinon démissionnez</strong>.</p>
+		</div>
+
+		<div class="card">		
+		    <img src="img/.svg" alt="" height="140">
+		    <h2>Science</h2>
+		    <p>Et vous pourrez dire : <strong>"J'y étais"</strong>.</p>
+	    </div>
+	    
+    <div class="card">
+		    <img src="img/.svg" alt="" height="140">
+		    <h2>Lancet</h2>
+		    <p>Une autre forme de <strong>développement personnel</strong> <img src="img/tss.png" height="20px"> .</p>
+		</div>
+      
+    <div class="card">
+		    <img src="img/.svg" alt="" height="140">
+		    <h2>PNAS</h2>
+		    <p>Une autre forme de <strong>développement personnel</strong> <img src="img/tss.png" height="20px"> .</p>
+		</div>
+
+	</div>
+
+	<div id="users">
+
+	</div>		
+	<div class="footer">
+    <p>Outil à l'usage <strong>exclusif</strong> des étudiants en santé toulousains prenant part à l'initiative flattencovid.org</p>
+	    
+	    <div class="list-inline">
+	    <ul>
+			<li>
+				<a href="#"><i class="fab fa-facebook-square" class="img-circle"></i></a>
+			</li>
+			<li>
+				<a href="#"><i class="fab fa-twitter-square" class="img-circle"></i></a>
+			</li>
+			<li>
+				<a href="#"><i class="fab fa-instagram" class="img-circle"></i></a>
+			</li>
+			<li>
+				<a href="#"><i class="fab fa-github" class="img-circle"></i></a>
+			</li>
+		</ul>
+		</div>
+
+	</div>
 
 </body>
+
 </html>
